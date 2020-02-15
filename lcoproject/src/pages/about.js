dd748bd4ac558ed5ed3f3a07ab28e5ce54050007
Ronak vanpariya <vanpariyar@gmail.com>
2020-02-16 01:10:03 +0530

    Learn to make about page

--- lcoproject/src/pages/about.js
+++ lcoproject/src/pages/about.js
@@ -7,25 +7,27 @@ import SEO from "../components/seo"
 import HeroSection from '../components/Reuseble/HeroSection'
 import Infoblock from '../components/Reuseble/Infoblock'
 import DualInfoblock from '../components/Reuseble/DualInfoblock'
+import Teamphotosection from '../components/About/Teamphotosection'
  
 
-const IndexPage = ({data}) => (
+const AboutPage = ({data}) => (
   <Layout>
     <SEO title="Home" />
     <HeroSection 
       img ={data.img.childImageSharp.fluid}
-      title= "I Write Code"
-      subtitle="Learn Code Online"
-      heroClass="hero-background"
+      title= "About LearnCode Online"
+      subtitle=""
+      heroClass="about-background"
     />
-    <Infoblock heading="About Us"/>
-    <DualInfoblock heading="Our Team" src="https://images.pexels.com/photos/1261427/pexels-photo-1261427.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260" /> 
+    <DualInfoblock heading="Message From CEO" src="https://images.pexels.com/photos/1261427/pexels-photo-1261427.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260" /> 
+    <Infoblock heading="About Vision"/>
+    <Teamphotosection />
   </Layout>
 )
 
 export const query = graphql`
 {
-  img: file(relativePath: { eq: "heromain.png" }) {
+  img: file(relativePath: { eq: "about.png" }) {
     childImageSharp {
       fluid {
         ...GatsbyImageSharpFluid_tracedSVG
@@ -36,4 +38,4 @@ export const query = graphql`
 } 
 `
 
-export default IndexPage
+export default AboutPage
